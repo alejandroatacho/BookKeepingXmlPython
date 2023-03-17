8cd74333371b2f19af1a292fccfcb473246e4419
Hinamizawa <hinamizawa.github@gmail.com>
2023-03-16 20:34:27 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,25 +1,61 @@
 #!/bin/bash
-# Path: run.sh
+#Path: run.sh
 
-PS3= 'Please enter your choice: '
-options=("Tree viewer" "Bank Options" "#" "Quit")
+while true; do
+PS3='Please enter your choice: '
+options=("Python Scripts" "Bank Options" "#" "Quit")
 select opt in "${options[@]}"
 do
-    case $opt in
-        "Tree viewer")
-            cd script
-            exec python tree_viewer.py
-            ;;
-        "Bank Options")
-            exec python book_keeping.py
-            ;;
-        "#")
-            echo $opt
-            # echo "you chose choice $REPLY which is $opt"
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
-    esac
+case $opt in
+"Python Scripts")
+PS3='Please choose a tree viewer option: '
+tree_options=("What is my xml/xslt path" "Convert XML to Website" "Tree Viewer" "Option 4")
+select tree_opt in "${tree_options[@]}"
+do
+case $tree_opt in
+"What is my xml/xslt path")
+# Do something for Option 1
+cd script
+python hunt_my_path.py
+read -p "Press any key to continue... "
+break
+;;
+"Convert XML to Website")
+cd script
+python make_website.py
+read -p "Press any key to continue... "
+break
+;;
+"Tree Viewer")
+# Do something for Option 3
+cd script
+python tree_viewer.py
+read -p "Press any key to continue... "
+break
+;;
+"Option 4")
+# Do something for Option 4
+break
+;;
+*) echo "Invalid option.";;
+esac
+done
+break
+;;
+"Bank Options")
+python book_keeping.py
+read -p "Press any key to continue... "
+break
+;;
+"#")
+echo $opt
+# echo "you chose choice $REPLY which is $opt"
+break
+;;
+"Quit")
+exit 0
+;;
+*) echo "Invalid option.";;
+esac
+done
 done
