b6fc4b066eb493331bd829d723454073964246ca
Hinamizawa <hinamizawa.github@gmail.com>
2023-04-01 20:51:49 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,4 @@
-//Make a JS file that converts to excel instead of python
-
-// function excelPrinter() {
-//    console.log("Begin your code")
-  
-
-// }
-//check your browser console log in inspect tool :v
-console.log('Hello I am connected!');
-
+// function to generate Excel file
 function generateExcel() {
   // Get the table element
   const table = document.querySelector('table');
@@ -24,6 +15,55 @@ function generateExcel() {
   // Generate the Excel file and download it
   XLSX.writeFile(workbook, 'transactions.xlsx');
 }
+
+// function to show income and loss transactions separately
 function showTransactionData() {
-  return 0;
+  // Get the table rows from the recent transactions table
+  const rows = document.querySelectorAll('table tr');
+
+  // Initialize empty arrays for income and loss transactions
+  const incomeTransactions = [];
+  const lossTransactions = [];
+
+  // Loop through the rows, and add the income and loss transactions to the respective arrays
+  for (let i = 1; i < rows.length; i++) {
+    const flow = rows[i].querySelector('td:last-child').textContent;
+    if (flow === 'Income') {
+      incomeTransactions.push(rows[i].cloneNode(true));
+    } else if (flow === 'Loss') {
+      lossTransactions.push(rows[i].cloneNode(true));
+    }
+  }
+
+  // Create a new container div to hold the income and loss tables
+  const dataContainer = document.createElement('div');
+  dataContainer.classList.add('data-container');
+
+  // Create the income table
+  const incomeTable = document.createElement('table');
+  const incomeHeader = document.createElement('tr');
+  incomeHeader.innerHTML = '<th>ID</th><th>Amount</th><th>Date</th><th>Description</th><th>Flow</th>';
+  incomeTable.appendChild(incomeHeader);
+
+  for (let i = 0; i < incomeTransactions.length; i++) {
+    incomeTable.appendChild(incomeTransactions[i]);
+  }
+
+  // Create the loss table
+  const lossTable = document.createElement('table');
+  const lossHeader = document.createElement('tr');
+  lossHeader.innerHTML = '<th>ID</th><th>Amount</th><th>Date</th><th>Description</th><th>Flow</th>';
+  lossTable.appendChild(lossHeader);
+
+  for (let i = 0; i < lossTransactions.length; i++) {
+    lossTable.appendChild(lossTransactions[i]);
+  }
+
+  // Add the income and loss tables to the container div
+  dataContainer.appendChild(incomeTable);
+  dataContainer.appendChild(lossTable);
+
+  // Add the container div to the DOM, next to the recent transactions table
+  const container = document.querySelector('.container');
+  container.insertBefore(dataContainer, container.querySelector('h3').nextSibling);
 }
